a3e2ee58e266e286a4c5c77eef5fa8ed70d22b1b
Mohammadreza Mohammadi <92263217+cryptoniubi@users.noreply.github.com>
2024-09-18 02:49:00 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#rpc="https://testnet-allora-rpc.lavenderfive.com"
-rpc="https://testnet-allora-rpc.lavenderfive.com"
+#rpc="https://rpc.ankr.com/allora_testnet"
+rpc="https://rpc.ankr.com/allora_testnet"
 
 read -p "Enter your worker index: " index
 read -p "Enter your mnemonic phrase: " mnemonic_phrase
